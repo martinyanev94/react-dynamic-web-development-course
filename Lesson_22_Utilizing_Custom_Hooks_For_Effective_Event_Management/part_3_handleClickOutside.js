da3c4763fff519838d869f5c2af9e596a8e87873
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-07 13:21:27 -0400

    Add Lesson_22_Utilizing_Custom_Hooks_For_Effective_Event_Management/part_3_handleClickOutside.js

---
+++ Lesson_22_Utilizing_Custom_Hooks_For_Effective_Event_Management/part_3_handleClickOutside.js
@@ -0,0 +1,9 @@
+const handleClickOutside = () => {
+    // Add your closing animation logic here
+    setIsOpen(false);
+};
+const handleClickOutside = () => {
+    setTimeout(() => {
+        setIsOpen(false);
+    }, 300); // assumes your animation duration is 300ms
+};
