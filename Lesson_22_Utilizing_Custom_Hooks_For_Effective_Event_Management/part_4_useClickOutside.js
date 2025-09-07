e60add4899db15696c8ca94dbfe1007417b46fb3
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-07 13:21:28 -0400

    Add Lesson_22_Utilizing_Custom_Hooks_For_Effective_Event_Management/part_4_useClickOutside.js

---
+++ Lesson_22_Utilizing_Custom_Hooks_For_Effective_Event_Management/part_4_useClickOutside.js
@@ -0,0 +1,18 @@
+function useClickOutside(refs, handler) {
+    useEffect(() => {
+        const listener = (event) => {
+            if (refs.some(ref => ref.current && ref.current.contains(event.target))) {
+                return;
+            }
+            handler(event);
+        };
+
+        document.addEventListener('mousedown', listener);
+        document.addEventListener('touchstart', listener);
+
+        return () => {
+            document.removeEventListener('mousedown', listener);
+            document.removeEventListener('touchstart', listener);
+        };
+    }, [refs, handler]);
+}
