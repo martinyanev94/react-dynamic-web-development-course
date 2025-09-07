d9f7ff0433c55adc35263a2b957ccdaf0db97569
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-07 13:21:59 -0400

    Add Lesson_3_Building_A_Single_Page_Application/part_4_HomeProductComponent.jsx

---
+++ Lesson_3_Building_A_Single_Page_Application/part_4_HomeProductComponent.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return <h1>Welcome to the Home Page</h1>;
+};
+
+export default Home;
+const Product = () => {
+    return <h1>This is the Product Page</h1>;
+};
+
+export default Product;
