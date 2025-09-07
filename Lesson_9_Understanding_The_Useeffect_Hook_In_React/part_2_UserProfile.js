562411f3029c65fca252a572986500921f6a6035
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-07 13:22:45 -0400

    Add Lesson_9_Understanding_The_Useeffect_Hook_In_React/part_2_UserProfile.js

---
+++ Lesson_9_Understanding_The_Useeffect_Hook_In_React/part_2_UserProfile.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+
+const UserProfile = () => {
+  const [user, setUser] = useState(null);
+  
+  useEffect(() => {
+    fetch('https://api.example.com/user')
+      .then(response => response.json())
+      .then(data => setUser(data));
+  }, []);
+
+  if (!user) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <p>{user.email}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
